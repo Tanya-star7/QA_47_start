781d3edd1b12a3b3c7e5eb5d52fddb660b82b9f4
Tanya-star7 <tatyana.star0107@gmail.com>
2025-05-19 22:23:52 +0300

    start css selectors

--- src/test/java/PhoneBookSelectors.java
+++ src/test/java/PhoneBookSelectors.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -15,6 +17,18 @@ public class PhoneBookSelectors {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         //homeWork
         FirstTest.pause(5);
+        WebElement butonLogin=driver.findElement(By.cssSelector("a[href='/login']");
+        butonLogin.click();
+        FirstTest.pause(5);
+        WebElement inputEmail=driver.findElement(By.cssSelector("input[placeholder='Email']");
+        inputEmail.click();
+        FirstTest.pause(5);
+        inputName.sendKeys("tatyana.star0107@gmail.com");
+        FirstTest.pause(2);
+
+
+
+
 
     }
 }
